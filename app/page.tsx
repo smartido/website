da5dc560c8988305a0a57866513c990ed02c1728
Sara Martínez <mrtnez.sara@gmail.com>
2023-06-27 11:17:48 +0200

    fead: add async to HomePage

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,7 @@ import { Header } from 'components/header';
 
 export const revalidate = 60;
 
-export default function HomePage() {
+export default async function HomePage() {
   return (
     <>
       <Header />
