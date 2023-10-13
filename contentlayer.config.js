1ecddf4c99abdce5a282252d6db1de2c4205c988
Sara Martínez <mrtnez.sara@gmail.com>
2023-10-13 10:33:32 +0200

    fix: image

--- contentlayer.config.js
+++ contentlayer.config.js
@@ -19,9 +19,7 @@ const computedFields = {
       datePublished: doc.publishedAt,
       dateModified: doc.publishedAt,
       description: doc.summary,
-      image: doc.image
-        ? `https://smartido.dev${doc.image}`
-        : `https://smartido.dev/og?title=${doc.title}`,
+      image: `https://smartido.dev/api/og?title=${doc.title}`,
       url: `https://smartido.dev/blog/${doc._raw.flattenedPath}`,
       author: {
         '@type': 'Person',
